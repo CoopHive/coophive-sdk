9dd0f993eead21fc349e98733926097b4706d7b9
Taylor Hulsmans <thulsmans.1133@gmail.com>
2024-08-12 13:48:37 +0200

    offchain boilerplate basic

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,18 @@
 import {
+  BrowserProvider,
+  FallbackProvider,
+  JsonRpcProvider,
+  JsonRpcSigner,
+
+} from "ethers";
+import type { Account, Chain, Client, Transport, PublicClient, WalletClient  } from "viem";
+import { TypeDataSigner } from '@ethereum-attestation-service/eas-sdk/dist/offchain/typed-data-handler';
+import {
+  OffchainAttestationParams,
+  OffchainAttestationOptions,
+  SignedOffchainAttestation,
   SchemaEncoder,
+  EAS,
   NO_EXPIRATION,
   ZERO_ADDRESS,
   ZERO_BYTES32,
@@ -18,6 +31,22 @@ export type Attestation = {
   }
 }
 
+type BuyStruct = {
+  supplier: `0x${string}`,
+  jobCost: bigint,
+  paymentToken: string,
+  creditsRequested: bigint,
+  collateralRequested: bigint,
+  offerDeadline: bigint,
+  jobDeadline: bigint,
+  arbitrationDeadline: bigint
+}
+
+export type BuyParams= {
+  schemaUID: `0x${string}`,
+  demander: `0x${string}`,
+  data: BuyStruct
+}
 
 export type BuyMessage  = [
   supplier: {name: string, value: any, type: string},
@@ -38,6 +67,8 @@ export type ValidationMessage = [
   isApproved: {name: string, value: any, type: string}
 ]
 
+
+
 export const buySchema: string  = "address supplier, uint256 jobCost, address paymentToken, uint256 creditsRequested, uint256 collateralRequested, uint256 offerDeadline, uint256 jobDeadline, uint256 arbitrationDeadline"  
 //export const  buySchemaUID: `0x${string}` = '0x5ab6e882c945d6d756ed93fff7ec7419fec64845cc0a480bfea2af053d685ee7'
 
@@ -55,6 +86,7 @@ const encodeMessage = (
 
 }
 
+
 const createBuyMessage = ({
   supplier,
   jobCost,
@@ -64,16 +96,7 @@ const createBuyMessage = ({
   offerDeadline,
   jobDeadline,
   arbitrationDeadline
-}:{
-  supplier: `0x${string}`
-  jobCost: bigint
-  paymentToken: string
-  creditsRequested: bigint
-  collateralRequested: bigint
-  offerDeadline: bigint
-  jobDeadline: bigint
-  arbitrationDeadline: bigint
-}) : BuyMessage => {
+}: BuyStruct ) : BuyMessage => {
   return [
     {name: 'supplier', value: supplier, type: 'address'},
     {name: 'jobCost', value: jobCost, type: 'uint256'},
@@ -95,16 +118,7 @@ const createBuyData = ({
   offerDeadline,
   jobDeadline,
   arbitrationDeadline
-}: {
-  supplier: `0x${string}`
-  jobCost: bigint
-  paymentToken: string
-  creditsRequested: bigint
-  collateralRequested: bigint
-  offerDeadline: bigint
-  jobDeadline: bigint
-  arbitrationDeadline: bigint
-}): `0x${string}` => {
+}: BuyStruct): `0x${string}` => {
   return encodeMessage(buySchema, createBuyMessage({
     supplier,
     jobCost,
@@ -233,3 +247,77 @@ export const createValidationAttestation = ({
 
 }
 
+
+// Offchain
+
+export const getEAS = (easContractAddress: `0x${string}`, signer: JsonRpcSigner) => {
+  return new EAS(easContractAddress, {signer})
+}
+
+
+export function clientToProvider(publicClient:PublicClient) {
+  const { chain, transport  } = publicClient;
+  if (!chain || !transport) return
+    const network = {
+      chainId: chain.id,
+      name: chain.name,
+      ensAddress: chain.contracts?.ensRegistry?.address,
+
+    };
+    if (transport.type === "fallback") {
+      const providers = (transport.transports as ReturnType<Transport>[]).map(
+        ({ value  }) => new JsonRpcProvider(value?.url, network)
+
+      );
+      if (providers.length === 1) return providers[0];
+      return new FallbackProvider(providers);
+
+    }
+    return new JsonRpcProvider(transport.url, network);
+
+}
+
+export function clientToSigner(walletClient: WalletClient): JsonRpcSigner | undefined {
+  const { account, chain, transport  } = walletClient;
+  console.log('acount', account)
+  console.log('chain', chain)
+  console.log('transport', transport)
+
+  if (!account || !chain || !transport) return 
+    const network = {
+      chainId: chain.id,
+      name: chain.name,
+      ensAddress: chain.contracts?.ensRegistry?.address,
+
+    };
+    const provider = new BrowserProvider(transport, network);
+    const signer = new JsonRpcSigner(provider, account.address);
+    return signer;
+}
+
+
+export const signOffchainBuyMessage = async (
+  easAddress: `0x${string}`,
+  schemaUID: `0x${string}`,
+  walletClient: WalletClient,
+  buyParams: BuyParams,
+  options?: OffchainAttestationOptions
+)  => {
+  const signer = clientToSigner(walletClient)
+  if (!signer) return
+  const eas = getEAS(easAddress, signer)
+  const offchain = await eas.getOffchain()
+
+  await offchain.signOffchainAttestation(
+    {
+      recipient: buyParams.demander,
+      expirationTime: 0n,
+      time: BigInt(Math.floor(Date.now() / 1000)),
+      revocable: true,
+      schema: schemaUID,
+      refUID: ZERO_BYTES32,
+      data: createBuyData(buyParams.data),
+    },
+    signer as TypeDataSigner,
+  )
+}
